d2a3a3f05aedb80caf16379f00aa8f40f8bebe87
wassim <wassim99f@gmail.com>
2024-11-01 00:25:27 +0200

    add public to fetch products from the url

--- src/Components/ProductPage.js
+++ src/Components/ProductPage.js
@@ -11,7 +11,7 @@ const ProductPage = () => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch('StoreApp/public/products.json')// for github pages the path should be puclic otherwise remove it
+    fetch('StoreApp/products.json')
       .then((response) => {
         if (!response.ok) {
           throw new Error('Network response was not ok');
