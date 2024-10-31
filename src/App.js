84a933d329def52c76fa09eb51aedbbe315f944e
wassim <wassim99f@gmail.com>
2024-10-31 23:46:30 +0200

    deploy to github pages

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Login />}  basename={process.env.PUBLIC_URL} />
         <Route path="/products" element={<ProductPage />} />
       </Routes>
     </Router>
