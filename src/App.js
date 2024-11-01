66f3715228c9a11d2ee372b06bbc02e6897a55e0
wassim <wassim99f@gmail.com>
2024-11-01 16:39:28 +0200

    Add to cart feature

--- src/App.js
+++ src/App.js
@@ -6,11 +6,18 @@ import ProductPage from './Components/ProductPage';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [cart, setCart] = useState([]);
+
+  const handleAddToCart = (product) => {
+    if (!cart.includes(product.id)) {
+      setCart((prevCart) => [...prevCart, product.id]);
+    }
+  };
   return (
     <Router>
       <Routes>
         <Route path="/StoreApp" element={<Login />} />
-        <Route path="/products" element={<ProductPage />} />
+        <Route path="/products" element={<ProductPage onAddToCart={handleAddToCart} cartCount={cart.length} cart={cart} />} />
       </Routes>
     </Router>
   );
